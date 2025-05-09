05db847e6388131d7dd26607838df32ff871fc30
Yafiakmal <yafiakmal45@gmail.com>
2025-05-09 18:21:28 +0700

    pesan commit untuk main

---
+++ git.push.main.gh-pages.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Exit kalau ada error
+set -e
+
+echo "📦 Commit perubahan di branch main..."
+git checkout main
+git add .
+git commit -m "${1:-'update content'}"
+
+echo "🚀 Deploy ke gh-pages..."
+git checkout gh-pages
+
+# Pastikan folder dist dari main diambil ulang
+git checkout main -- dist
+
+git add .
+git commit -m "${2:-'deploy dist to gh-pages'}"
+git push origin gh-pages
+
+# Kembali ke main
+git checkout main
+git push origin main
+echo "✅ Selesai!"
