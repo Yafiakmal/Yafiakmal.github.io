15705b3d592681a33730301233aad0249e6838e5
Yafiakmal <yafiakmal45@gmail.com>
2025-05-12 15:15:14 +0700

    gh-pages

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-// tailwind.config.js
-export default {
-    content: ["./index.html", "./src/**/*.{vue,js,ts}"],
-    theme: {
-      extend: {}
-    },
-    plugins: [require("tailwind-scrollbar-hide")]
-  }
-  
