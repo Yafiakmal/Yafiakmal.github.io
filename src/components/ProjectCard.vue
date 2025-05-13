a1952140624ae36477bf60a794d6bc4678396c87
Yafiakmal <yafiakmal45@gmail.com>
2025-05-13 07:19:48 +0700

    template created

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="bg-gray-800 rounded-xl shadow-md overflow-hidden hover:scale-[1.02] transition-transform duration-200">
+      <!-- Image -->
+      <div class="h-48 w-full">
+        <img
+          :src="imagePath"
+          alt="my projects image"
+          class="object-cover w-full h-full"
+        />
+      </div>
+  
+      <!-- Text Content -->
+      <div class="p-4 space-y-2">
+        <h3 class="text-xl font-semibold text-white">{{ title }}</h3>
+        <p class="text-gray-300 text-sm">{{ description }}</p>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  defineProps({
+    title: String,
+    description: String,
+    imagePath: String
+  })
+  </script>
+  
