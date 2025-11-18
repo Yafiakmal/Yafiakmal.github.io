2acdf033ffd3829e944f3671a89782130a62a1d3
Yafiakmal <yafiakmal45@gmail.com>
2025-11-18 07:51:33 +0700

    add Header, and more hero

---
+++ src/component/Header/Header.tsx
@@ -0,0 +1,21 @@
+export default function Header() {
+    return (
+        <>
+            <div className="header px-2 h-[70px] w-screen bg-blue-200 flex justify-between">
+                <div className="logo h-full w-[20%] bg-green-100 flex justify-center items-center">
+                    <div className="img  bg-green-300">
+                        <h1>MYA</h1>
+                    </div>
+                </div>
+                <div className="links px-2 h-full w-full bg-green-200 flex justify-end items-center gap-3">
+                    <li className="nav px-2 w-full md:w-[50%] bg-green-300 flex justify-between">
+                        <ul><a href="#">link1</a></ul>
+                        <ul><a href="#">link2</a></ul>
+                        <ul><a href="#">link3</a></ul>
+                        <ul><a href="#">link4</a></ul>
+                    </li>
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ src/component/Hero/MyProject.tsx
@@ -0,0 +1,11 @@
+export default function MyProject() {
+    return (
+        <>
+            <section className="bg-[#1D546C] w-screen h-[530px] px-[60px] pt-[30px] flex flex-col font-mono">
+                <div className="title w-full h-fit flex justify-center">
+                    <h1 className="text-2xl font-black text-blue-50">Latest Project</h1>
+                </div>
+            </section>
+        </>
+    )
+}
